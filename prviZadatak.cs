4762526a082ff6d102599c665cd0a2b03eaa5468
rubenhamza1 <90246098+rubenhamza1@users.noreply.github.com>
2021-09-21 11:17:57 +0200

    Create prviZadatak.cs

---
+++ prviZadatak.cs
@@ -0,0 +1,18 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		int a = 100;
+		int b = -1000;
+		char c = 'a';
+		float d = 3.123f;
+		float e = 3.123456789f;
+		bool f = true;
+		string g = "true";
+		
+		
+		Console.WriteLine("Hello World");
+	}
+}
